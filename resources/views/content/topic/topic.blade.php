9f6aaee3bb51901c3fd5fa5608c03d4fa7720281
shbrnsabrina <atikahss2006@gmail.com>
2024-10-16 18:21:05 +0700

    unfinished database for topic

--- resources/views/content/topic/topic.blade.php
+++ resources/views/content/topic/topic.blade.php
@@ -16,6 +16,38 @@
 
 @section('content')
 <div class="community d-flex justify-content-around">
+
+  <div class="create-topic">
+    <button type="button" class="btn btn-primary btn-m" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">CREATE</button>
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title fs-5" id="exampleModalLabel">CREATE TOPIC</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <form method="POST" action="{{ route('posts.store') }}" enctype="multipart/form-data">
+              @csrf
+              <div class="mb-3">
+                <label for="message-text" class="col-form-label">Title: </label>
+                <textarea class="form-control" id="topic-title" name="topic" placeholder="Let's create a topic!"></textarea>
+              </div>
+              <div class="mb-3">
+                <label for="message-text" class="col-form-label">Description: </label>
+                <textarea class="form-control" id="topic-title" name="topic" placeholder="Type your description."></textarea>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary">Create</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
   <div class="card" style="width: 18rem;">
     <img src="{{ asset('assets/img/backgrounds/graphics.jpg') }}" class="card-img-top" alt="...">
     <div class="card-body">
